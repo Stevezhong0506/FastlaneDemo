5ab331cfb64530de147b1c6ec15bc1ef84a5f96f
zhongxueyu <zhongxueyu@zhongxueyudeimac.local>
2018-05-24 16:05:59 +0800

    Initial Commit

---
+++ FastlaneDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastlaneDemo
+//
+//  Created by zhongxueyu on 2018/5/24.
+//  Copyright © 2018年 zhongxueyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
